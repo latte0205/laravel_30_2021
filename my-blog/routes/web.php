a8e0b96d02dd58c2e4dad77e79de241f483733c7
Sean <latte0205@gmail.com>
2021-01-21 18:29:23 +0800

    Day1-Day4

--- my-blog/routes/web.php
+++ my-blog/routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// post 新增, put patch 修改, get 查詢, delete 刪除
+
+
+// Route::get('/about', function () {
+//     return 'hi';
+// });
+
+Route::get('/cats/{id}/{name}', function ($id, $name) {
+    return "我是第幾 ".$id." 號的貓 ". $name;
+});
+
+Route::get('/about', 'WelcomeController@about');
